afb9573e63bba1eb18a2c53906679917df8b8f3e
Im2Slothy <69122728+Im2Slothy@users.noreply.github.com>
2022-03-10 13:28:09 -0600

    Create meme.py
    lul

---
+++ meme.py
@@ -0,0 +1,12 @@
+import os 
+import time
+import browser
+import webrowser
+def main():
+  print("You have fallen for my trap! I now know you're deepest and darkest fear!")
+  sleep(5)
+  meme := 5
+    
+    for meme > 1 {
+      webbrowser.open_new_tab('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+      
