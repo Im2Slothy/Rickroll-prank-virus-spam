cf0461d54f56c7807b1a8cdd65fb696ba169b81d
Im2Slothy <69122728+Im2Slothy@users.noreply.github.com>
2022-03-10 13:33:59 -0600

    Wrong file.

--- meme.go
+++ meme.go
@@ -1,12 +1,21 @@
-import os 
-import time
-import browser
-import webrowser
-def main():
-  print("You have fallen for my trap! I now know you're deepest and darkest fear!")
-  sleep(5)
-  meme := 5
-    
-    for meme > 1 {
-      webbrowser.open_new_tab('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
-      
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/pkg/browser"
+)
+
+func main() {
+	fmt.Print("You have just fallen into my trap... Sucks to be you!")
+	time.Sleep(5 * time.Second)
+	spam := 10
+
+	for spam > 0 {
+		browser.OpenURL("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+		spam--
+
+	}
+
+}
